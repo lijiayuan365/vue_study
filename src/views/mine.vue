59cbc4f4025a1fe8e0d50d1d4fc4effc4b7d1cab
lijiayuan365 <1399208411@qq.com>
2018-07-26 12:24:56 +0800

    first commit

---
+++ src/views/mine.vue
@@ -0,0 +1,30 @@
+<!--  -->
+<template>
+    <div>
+      mine
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {};
+        },
+
+        components: {},
+
+        computed: {},
+
+        created() {
+        },
+
+        mounted() {
+        },
+
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
